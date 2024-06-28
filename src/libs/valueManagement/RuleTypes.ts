cc66b60a0e3522503c1936d91e1397a2478eec1e
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2024-06-28 13:18:36 -0600

    Extraction for Simplification

--- src/libs/valueManagement/RuleTypes.ts
+++ src/libs/valueManagement/RuleTypes.ts
@@ -1,3 +1,11 @@
 import { Rule } from "./rules";
 
+export type ValueDataType = { [x: string]: unknown };
+
 export type RuleList<T> = { [x in keyof T]: Rule<T> };
+
+export type KeyValueRuleType<T> = {
+    key: string;
+    value: T[keyof T];
+    rule: Rule<T>;
+};
--- src/libs/valueManagement/ValueManager.ts
+++ src/libs/valueManagement/ValueManager.ts
@@ -1,42 +1,21 @@
 import topSort, { TopSortableType } from "../topologicalSort";
 import { Rule } from "./rules";
-import { RuleList } from "./RuleTypes";
+import { KeyValueRuleType, RuleList, ValueDataType } from "./RuleTypes";
 
-const parseChanges = <T extends { [x: string]: unknown }>(
+const parseChanges = <T extends ValueDataType>(
     curr: T,
     changes: Partial<T>,
     rules: RuleList<T>,
 ): Partial<T> => {
-    let newCurr = { ...curr };
-
-    const changesAndRules: TopSortableType[] = Object.entries(
+    const changesAndRules: TopSortableType[] = convertToTopSortable(
         changes,
-    ).map<TopSortableType>(([key]) => {
-        return {
-            label: key as string,
-            dependents: rules[key as keyof T].requirements() as string[],
-        };
-    });
+        rules,
+    );
 
     const rulesOrder = topSort(changesAndRules);
 
-    rulesOrder
-        .map((r) => {
-            return {
-                key: r.label,
-                value: changes[r.label],
-                rule: rules[r.label],
-            };
-        })
-        .forEach((v) => {
-            const change = parseChange(
-                newCurr,
-                v.key,
-                v.value as T[keyof T],
-                v.rule,
-            );
-            newCurr = { ...newCurr, ...change };
-        });
+    const keyValueRule = convertToKeyValueRule(rulesOrder, changes, rules);
+    const newCurr = processChanges(keyValueRule, curr);
 
     return newCurr;
 };
@@ -49,6 +28,56 @@ export const parseChange = <T extends { [x: string]: unknown }>(
 ) => {
     const allowed = rule.test(curr, value);
     return allowed ? { [key]: value } : {};
+};
+
+const convertToTopSortable = <T extends ValueDataType>(
+    changes: Partial<T>,
+    rules: RuleList<T>,
+) => {
+    return Object.keys(changes).map<TopSortableType>((key) => {
+        return {
+            label: key,
+            dependents: rules[key as keyof T].requirements() as string[],
+        };
+    });
+};
+
+const convertToKeyValueRule = <T extends ValueDataType>(
+    x: TopSortableType[],
+    changes: Partial<T>,
+    rules: RuleList<T>,
+): KeyValueRuleType<T>[] => {
+    return x.map((r) => {
+        const newValue: T[string] | undefined = changes[r.label];
+        const newRule = rules[r.label];
+
+        if (newValue === undefined) throw new Error("Why can I do this");
+
+        return {
+            key: r.label,
+            value: newValue,
+            rule: newRule,
+        };
+    });
+};
+
+const processChanges = <T extends ValueDataType>(
+    x: KeyValueRuleType<T>[],
+    currData: T,
+) => {
+    let newCurr = { ...currData };
+
+    x.forEach((v) => {
+        const change = parseChange(
+            newCurr,
+            v.key,
+            v.value as T[keyof T],
+            v.rule,
+        );
+        newCurr = { ...newCurr, ...change };
+    });
+
+    return newCurr;
 };
 
 export default parseChanges;
